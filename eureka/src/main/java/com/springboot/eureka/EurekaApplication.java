c833bdcbcd4b35114e5485305fe6f0484745d56c
wuwen901 <1195082140@qq.com>
2020-05-07 19:19:26 +0800

    eureka

---
+++ eureka/src/main/java/com/springboot/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.springboot.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wuwen
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
